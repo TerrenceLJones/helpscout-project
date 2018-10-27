60aa740fd4c916f8444e0087d4453eb0b82d72cb
Terrence L. Jones <terrenceljones805@gmail.com>
2018-10-27 03:53:44 -0500

    Add book loading to mock api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -18,6 +18,33 @@ const createBook = data => {
   });
 };
 
+const loadAll = () => {
+  return new Promise((resolve, reject) => {
+    const parsedData = JSON.parse(localStorage.getItem('bookData'));
+    setTimeout(() => resolve(parsedData), 2000)
+  });
+}
+
+const loadOne = id => {
+  return new Promise((resolve, reject) => {
+    if(!id) {
+      reject(TypeError('Must provide an id.'));
+    }
+
+    const parsedBookData = JSON.parse(localStorage.getItem('bookData'));
+    const foundItem = parsedBookData.find(book => book.id === id);
+
+    setTimeout(() => {
+      if(!foundItem) {
+        reject('Not found.')
+      }
+      resolve(foundItem)
+    }, 2000)
+  });
+}
+
 export {
-  createBook
+  createBook,
+  loadAll,
+  loadOne
 };
