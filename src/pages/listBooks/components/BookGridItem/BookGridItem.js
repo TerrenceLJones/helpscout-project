7f20c094ec20a65db4b381a5fb79054ac3a67ea7
Terrence L. Jones <terrenceljones805@gmail.com>
2018-10-28 23:36:18 -0700

    Update Grid and List component for ListBooks

---
+++ src/pages/listBooks/components/BookGridItem/BookGridItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import styles from './BookGridItem.module.css';
+
+const BookGridItem = function Book({ book }) {
+  const { id, image: imagePreviewUrl, title } = book;
+  const backgroundImage = imagePreviewUrl || 'https://dummyimage.com/448x400/000/fff.png&text=Add+a+Book+Cover';
+
+  return (
+    <li className={ `${styles.bookGridItem } card col-md-3 col-sm-12 mb-4 shadow-sm` }>
+      <Link className={ styles.bookGridItemLink } to={ `/books/${id}` }>
+        <div
+          className={ styles.bookGridItemImage }
+          style={ { backgroundImage: `url("${backgroundImage}")` } }
+        />
+        <h3>{ title }</h3>
+      </Link>
+    </li>
+  )
+};
+
+BookGridItem.propTypes = {
+  book: PropTypes.object
+}
+
+export default BookGridItem;
---
+++ src/pages/listBooks/components/BookListItem/BookListItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import styles from './BookListItem.module.css';
+const BookListItem = function Book({ book }) {
+  const { author, category, id, image: imagePreviewUrl, title } = book;
+  const backgroundImage = imagePreviewUrl || 'https://dummyimage.com/448x400/000/fff.png&text=Add+a+Book+Cover';
+
+  return (
+    <li className={`${styles.bookListItem } card col-12 mb-4`}>
+      <Link to={ `/books/${id}` }>
+        <div className="row">
+          <div className={`${styles.bookListImageContainer} col-6` }>
+            <div style={ {
+              backgroundImage: `url("${backgroundImage}")`,
+              backgroundSize: 'cover',
+              display: 'block',
+              height: '100%'
+            } } />
+          </div>
+          <div className="card-body col-6">
+            <h3 className={ styles.bookMeta }>{ author }</h3>
+            <h3 className={ styles.bookMeta }>{ title }</h3>
+            <p className={ styles.bookMeta }>{ category }</p>
+          </div>
+        </div>
+      </Link>
+    </li>
+  )
+};
+
+BookListItem.propTypes = {
+  book: PropTypes.object
+}
+
+export default BookListItem;
