f699f5c983c584ca5ef9e14908f72ee3cacfd5c1
Terrence L. Jones <terrenceljones805@gmail.com>
2018-10-29 13:44:37 -0700

    Add placeholder integration specs

---
+++ src/integrations.test.js
@@ -0,0 +1,13 @@
+import App from 'App';
+
+describe('Add a new book', () => {
+    it('saves book and navigates to book details page', () => {});
+});
+
+describe('Editing a book', () => {
+    it('saves book and navigates to book details page', () => {});
+});
+
+describe('Deleting a book', () => {
+    it('deletes the book and navigates to book list page', () => {});
+});
