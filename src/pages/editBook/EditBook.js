2347752582f32f3709ad88137cbff2a7db44ec26
Terrence L. Jones <terrenceljones805@gmail.com>
2018-10-28 19:19:33 -0700

    Update EditBook component

--- src/pages/editBook/EditBook.js
+++ src/pages/editBook/EditBook.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Header from 'components/Header';
 import ErrorDisplay from 'components/ErrorDisplay';
 import Loader from 'components/Loader';
 import { UnknownBook } from 'components/UnknownBook';
@@ -14,7 +15,6 @@ import { apiSelectors } from 'state/modules/api'
 const resultsLoadingSelector = apiSelectors.createLoadingSelector([booksTypes.LOAD_BOOKS_REQUEST]);
 const resultsErrorSelector = apiSelectors.createErrorMessageSelector([booksTypes.LOAD_BOOKS_FAILURE]);
 
-
 class EditBook extends Component {
   static propTypes = {
     book: PropTypes.object
@@ -24,30 +24,30 @@ class EditBook extends Component {
       this.props.loadBook();
   }
 
-  handleSubmit = (bookValues) => {
+  onSaveBook = (bookValues) => {
     this.props.updateBook(bookValues)
       .then(({ payload: { data: newBook } }) => {
         this.props.history.replace(`/books/${newBook.id}`);
       });
   }
 
-  handleDelete = (bookId) => {
+  onDeleteBook = (bookId) => {
     this.props.deleteBook(bookId)
       .then(() => this.props.history.replace('/books'));
   }
 
   getHeader() {
     return (
-      <header>
+      <Header>
         <h1>Edit Book</h1>
-        <Link to={ `/books/${this.props.book.id}` }>Cancel</Link>
-      </header>
+        <Link className="btn btn-secondary" to={ `/books/${this.props.book.id}` }>Cancel</Link>
+      </Header>
     );
   }
 
   render() {
     if(this.props.error) {
-    return <ErrorDisplay />;
+      return <ErrorDisplay />;
     }
 
     if(this.props.isLoading) {
@@ -59,15 +59,17 @@ class EditBook extends Component {
     }
 
     return (
-      <div>
+      <React.Fragment>
         { this.getHeader() }
 
-        <EditBookForm
-          book={ this.props.book }
-          handleDelete={ this.handleDelete }
-          handleSubmit={ this.handleSubmit }
-        />
-      </div>
+        <div className="container">
+          <EditBookForm
+            book={ this.props.book }
+            onDelete={ this.onDeleteBook }
+            onSubmit={ this.onSaveBook }
+          />
+        </div>
+      </React.Fragment>
     );
   }
 };
