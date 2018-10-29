8ef6916a354a297e2a07f98d23dab191b286be50
Terrence L. Jones <terrenceljones805@gmail.com>
2018-10-28 23:36:38 -0700

    Clean up in UnknownBook

--- src/components/UnknownBook.js
+++ src/components/UnknownBook.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const UnknownBook = () => (
+const UnknownBook = () => (
   <div>
     <p>Oops! It seems like this book couldn&#39;t be found.</p>
     <Link to="/books">Go to Book List</Link>
     <Link to="/books/new">Create a new book</Link>
   </div>
 );
+
+export default UnknownBook;
